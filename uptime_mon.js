d808dcb9e1e3811bc7ea3a7c98f9641aadd76c2c
Damian Davila <damiandavilag@gmail.com>
2021-12-10 18:19:16 -0500

    Check for site title

--- uptime_mon.js
+++ uptime_mon.js
@@ -12,15 +12,15 @@ var version_number = "1.1";
 
 var fs = require('fs');
 var configJson = __dirname + '/uptime-config.json';
-var listConfig = require( configJson );
+var listConfig = require(configJson);
 
 var appConfigJson = __dirname + '/app-config.json';
-var appConfig = require( appConfigJson );
+var appConfig = require(appConfigJson);
 
 //  Server logging
-var logFileBase = __dirname + '/logs/logFileUptime' 
+var logFileBase = __dirname + '/logs/logFileUptime'
 var logFile = logFileBase + '.0';
-var maxLogVer = 6;  // 0-based
+var maxLogVer = 6; // 0-based
 var log = function(msg) {
     var d = new Date();
     console.log(d.toUTCString() + ' :: ' + msg);
@@ -38,26 +38,26 @@ rotateLogs();
 var Xray = require('x-ray');
 var xray = Xray({
     filters: {
-      trim: function (value) {
-        return typeof value === 'string' ? value.trim() : value
-      },
-      reverse: function (value) {
-        return typeof value === 'string' ? value.split('').reverse().join('') : value
-      },
-      slice: function (value, start , end) {
-        return typeof value === 'string' ? value.slice(start, end) : value
-      },
-      extractRating: function (value, ratingClass, start, end) {
-        if (typeof(value) === 'string') {
-            var strt = value.indexOf(ratingClass) + start;
-            var endd = strt + end;
-            return (value.slice(strt, endd));
-        } else { 
-            return value;
+        trim: function(value) {
+            return typeof value === 'string' ? value.trim() : value
+        },
+        reverse: function(value) {
+            return typeof value === 'string' ? value.split('').reverse().join('') : value
+        },
+        slice: function(value, start, end) {
+            return typeof value === 'string' ? value.slice(start, end) : value
+        },
+        extractRating: function(value, ratingClass, start, end) {
+            if (typeof(value) === 'string') {
+                var strt = value.indexOf(ratingClass) + start;
+                var endd = strt + end;
+                return (value.slice(strt, endd));
+            } else {
+                return value;
+            }
         }
-      }
     }
-  });
+});
 
 // var GoogleLocations = require('google-locations');
 // var locations = new GoogleLocations('AIzaSyCkSNYioETcCDZPvWZ8meZLpeEzA4rUqAE');
@@ -82,26 +82,26 @@ function mainLoop() {
     var badMsg = "";
     var fetches = [];
     var uptimeEmail = "control@moventisusa.com";
-	var sites = Object.keys(listConfig.client);
-	sites.forEach( function(site) {
-        fetches.push( fetchURL( listConfig.client[site].url, listConfig.client[site].clientName ) ); 
+    var sites = Object.keys(listConfig.client);
+    sites.forEach(function(site) {
+        fetches.push(fetchURL(listConfig.client[site].url, listConfig.client[site].clientName, listConfig.client[site].title));
     });
-    Promise.each( fetches, function( fetchResult ) {
-        if ( fetchResult.fetchedTitle ) {
+    Promise.each(fetches, function(fetchResult) {
+        if (fetchResult.fetchedTitle) {
             goodMsg += fetchResult.name + " is up; title is " + fetchResult.fetchedTitle + "<br/>";
         } else {
-            badMsg += fetchResult.name + " is NOT up.<br/>";
+            badMsg += fetchResult.name + " is NOT up or title does not match.<br/>";
         }
-  	}).then( function(){
-                
-                if ( badMsg ) {
-                    alertError( transporter, badMsg );
-                } else {
-                    if ( goodMsg ) {
-                        sendNotificationEmail( transporter, uptimeEmail, goodMsg );
-                    }
-                }
-        });
+    }).then(function() {
+
+        if (badMsg) {
+            alertError(transporter, badMsg);
+        } else {
+            if (goodMsg) {
+                sendNotificationEmail(transporter, uptimeEmail, goodMsg);
+            }
+        }
+    });
     log("Ending mainLoop(); version: " + version_number);
 };
 
@@ -110,76 +110,81 @@ mainLoop();
 // ===============================  
 //  Fetch the target URL and validate it's "upness"
 // ===============================
-function fetchURL (url, clientName) {
+function fetchURL(url, clientName, clientTitle) {
     return new Promise(function(resolve, reject) {
         xray(url, 'title')(function(err, title) {
             if (err) {
                 log(err);
-                resolve( {fetchedTitle: "", name: clientName} );
+                resolve({ fetchedTitle: "", name: clientName });
             }
-            resolve( {fetchedTitle: title, name: clientName} );
+            if (title && title.trim() == clientTitle) {
+                resolve({ fetchedTitle: title, name: clientName });
+            } else {
+                log("Title does not match");
+                resolve({ fetchedTitle: "", name: clientName });
+            }
         });
         //return title;
     });
 };
- 
+
 // ===============================
 //  Send notification email to client
 // ===============================
-function sendNotificationEmail (transporter, wpTargetEmail, msg) {
-    
-      // setup e-mail data with unicode symbols 
-      var d = new Date();
-      var wpEmailConfig = "<img style='max-width: 150px; height: auto;' src='https://onehandoff.com/wp-content/uploads/2017/03/One-Hand-Off-logo-vertical-2.png'>" +
-                            "<h2>Client Site Uptime Status</h2>" +
-                            "<p>" + msg + "</p>" +
-                            "<p>&copy; 2017 One Hand Off";
-    
-      var mailOptions = {
-          from: "OHO Uptime Monitor <uptime@onehandoff.com>", // sender address **MUST be registered in Webfaction email system 
-          to: wpTargetEmail, 
-          subject: "One Hand Off uptime monitoring status " + d.toString().substr(0,15), 
-          html: wpEmailConfig // html body 
-      };
-        //log("mail options:", mailOptions); // TESTING
-    
-        transporter.sendMail(mailOptions, function(error, info){
-          if(error){
-              return log(error);
-          }
-          log('Notification sent: ' + info.response);
-          return 0;
-      });
+function sendNotificationEmail(transporter, wpTargetEmail, msg) {
+
+    // setup e-mail data with unicode symbols 
+    var d = new Date();
+    var wpEmailConfig = "<img style='max-width: 150px; height: auto;' src='https://onehandoff.com/wp-content/uploads/2017/03/One-Hand-Off-logo-vertical-2.png'>" +
+        "<h2>Client Site Uptime Status</h2>" +
+        "<p>" + msg + "</p>" +
+        "<p>&copy; 2017 One Hand Off";
+
+    var mailOptions = {
+        from: "OHO Uptime Monitor <uptime@onehandoff.com>", // sender address **MUST be registered in Webfaction email system 
+        to: wpTargetEmail,
+        subject: "One Hand Off uptime monitoring status " + d.toString().substr(0, 15),
+        html: wpEmailConfig // html body 
     };
-    
+    //log("mail options:", mailOptions); // TESTING
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return log(error);
+        }
+        log('Notification sent: ' + info.response);
+        return 0;
+    });
+};
+
 // =====================
 //  Alert admin via email for any error conditions
 // =====================
-function alertError (transporter, errMessage) {
-   
-  // setup e-mail data with unicode symbols 
-  var mailOptions = {
-  	from: 'OHO Uptime Monitor <uptime@onehandoff.com>', // sender address **MUST be registered in Webfaction email system 
-      to: 'damiandavila@yahoo.com, control@onehandoff.com, 9544657537@txt.att.net', // list of receivers 
-      subject: 'OHO uptime monitoring error encountered', // Subject line 
-      text: 'Houston, we have a problem: ' + errMessage, // plaintext body 
-      html: '<b>Houston we have a problem: </b>' + errMessage // html body 
-  };
-   
-  // send mail with defined transport object 
-  transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          return log(error);
-      }
-      log('Alert error sent: ' + errMessage + ' Send status: ' + info.response);
-      return;
-  });
+function alertError(transporter, errMessage) {
+
+    // setup e-mail data with unicode symbols 
+    var mailOptions = {
+        from: 'OHO Uptime Monitor <uptime@onehandoff.com>', // sender address **MUST be registered in Webfaction email system 
+        to: 'damiandavila@yahoo.com, control@onehandoff.com, 9544657537@txt.att.net', // list of receivers 
+        subject: 'OHO uptime monitoring error encountered', // Subject line 
+        text: 'Houston, we have a problem: ' + errMessage, // plaintext body 
+        html: '<b>Houston we have a problem: </b>' + errMessage // html body 
+    };
+
+    // send mail with defined transport object 
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return log(error);
+        }
+        log('Alert error sent: ' + errMessage + ' Send status: ' + info.response);
+        return;
+    });
 };
 
 // ===============================
 //  Set up email transport
 // ===============================
-function setupTransport (nodemailer) {
+function setupTransport(nodemailer) {
 
     // create reusable transporter object using the default SMTP transport 
     var smtpConfig = {
@@ -192,21 +197,21 @@ function setupTransport (nodemailer) {
         }
     };
     return nodemailer.createTransport(smtpConfig);
-  }; 
+};
 
 // ===============================
 //  Do log file backups
 // ===============================
-function rotateLogs () {
-    for(var i = maxLogVer; i > 0; i--) {
+function rotateLogs() {
+    for (var i = maxLogVer; i > 0; i--) {
         try {
-            var logFrom = logFileBase + '.' + (i-1).toString();
+            var logFrom = logFileBase + '.' + (i - 1).toString();
             var logTo = logFileBase + '.' + i.toString();
-            fs.renameSync(logFrom, logTo);          
+            fs.renameSync(logFrom, logTo);
         } catch (error) {
-            log(error);      
-        }          
+            log(error);
+        }
     };
     log('Rotated log files');
-    return;            
-}; 
+    return;
+};
